34cdbc630cb56e2356cc0d475bc4d33d516b223f
zhakina90 <zhakina90@gmail.com>
2018-11-26 00:13:34 -0600

    Added train link to my protfolio. Added app.js to create function for links. Changed images. Deleted unused images.

---
+++ assets/javascript/app.js
@@ -0,0 +1,21 @@
+function relocate_game() {
+    location.href = "https://zhakina90.github.io/Psychic-Game/";
+}
+function relocate_game1() {
+    location.href = "https://zhakina90.github.io/unit-4-game/";
+}
+
+function relocate_game2() {
+    location.href = "https://zhakina90.github.io/TriviaGame/";
+}
+
+function relocate_giphy() {
+    location.href = "https://zhakina90.github.io/Giphy/";
+}
+function relocate_train() {
+    location.href = "https://zhakina90.github.io/train-homework/";
+}
+// relocate_game();
+// relocate_game1();
+// relocate_giphy();
+// relocate_train();
